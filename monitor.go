8459383e6163eb9762fa798a215a47041337e082
Henrique Cecconi <henrique.cecconi@gmail.com>
2021-11-28 16:48:34 -0300

    Dealing with the commands using if, else if and else

--- monitor.go
+++ monitor.go
@@ -9,13 +9,22 @@ func main() {
 	fmt.Println("Hello,", name)
 	fmt.Println("This is the version", version, "of the program")
 
+	fmt.Println("0 - Exit")
 	fmt.Println("1 - Start monitoring sites")
 	fmt.Println("2 - Show logs")
-	fmt.Println("3 - Exit")
 
 	var command int
 	fmt.Scan(&command)
 	//fmt.Scanf("%d", &command)
-
 	fmt.Println("The chosen command is:", command)
+
+	if command == 0 {
+		fmt.Println("Exiting the program")
+	} else if command == 1 {
+		fmt.Println("Monitoring...")
+	} else if command == 2 {
+		fmt.Println("Exhibiting logs...")
+	} else {
+		fmt.Println("I don't know this command")
+	}
 }
