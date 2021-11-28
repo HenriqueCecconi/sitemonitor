badefed1f27e1d5a202c7a8229a9c35aab932d8a
Henrique Cecconi <henrique.cecconi@gmail.com>
2021-11-28 19:08:03 -0300

    Creating functions to show the introduction and the menu, and read the input, to clean my code

--- monitor.go
+++ monitor.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	name := "Harry"
-	version := 1.1
 
-	fmt.Println("Hello,", name)
-	fmt.Println("This is the version", version, "of the program")
+	showIntroduction()
 
-	fmt.Println("0 - Exit")
-	fmt.Println("1 - Start monitoring sites")
-	fmt.Println("2 - Show logs")
+	showMenu()
 
-	var command int
-	fmt.Scan(&command)
-	fmt.Println("The chosen command is:", command)
+	command := readInput()
 
 	switch command {
 	case 0:
@@ -27,4 +20,24 @@ func main() {
 	default:
 		fmt.Println("I don't know this command")
 	}
+}
+
+func showIntroduction() {
+	name := "Harry"
+	version := 1.1
+	fmt.Println("Hello,", name)
+	fmt.Println("This is the version", version, "of the program")
+}
+
+func showMenu() {
+	fmt.Println("0 - Exit")
+	fmt.Println("1 - Start monitoring sites")
+	fmt.Println("2 - Show logs")
+}
+
+func readInput() int {
+	var inputRecieved int
+	fmt.Scan(&inputRecieved)
+	fmt.Println("The chosen command is:", inputRecieved)
+	return inputRecieved
 }
