4ef9a2abea7879019c462c0c1951a2ba1eb42f7e
Henrique Cecconi <henrique.cecconi@gmail.com>
2021-11-28 20:26:10 -0300

    Making the Monitoring function, that access the site and retrieves it's Status Code

--- monitor.go
+++ monitor.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
@@ -14,7 +17,7 @@ func main() {
 	case 0:
 		fmt.Println("Exiting the program")
 	case 1:
-		fmt.Println("Monitoring...")
+		starMonitoring()
 	case 2:
 		fmt.Println("Exhibiting logs...")
 	default:
@@ -24,7 +27,7 @@ func main() {
 
 func showIntroduction() {
 	name := "Harry"
-	version := 1.1
+	version := 1.2
 	fmt.Println("Hello,", name)
 	fmt.Println("This is the version", version, "of the program")
 }
@@ -40,4 +43,16 @@ func readInput() int {
 	fmt.Scan(&inputRecieved)
 	fmt.Println("The chosen command is:", inputRecieved)
 	return inputRecieved
+}
+
+func starMonitoring() {
+	fmt.Println("Monitoring...")
+	// This site give me a random status code every time I access it
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("The site", site, "was successfully accessed!")
+	} else {
+		fmt.Println("Couldn't access", site, "smoothly. Status Code:", resp.StatusCode)
+	}
 }
